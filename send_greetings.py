23fc2fd1dbb98421d444bf69eee6ad78ec453a76
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2021-07-23 16:32:43 +0500

    Update send_greetings.py

--- send_greetings.py
+++ send_greetings.py
@@ -1,13 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 from selenium import webdriver
 from time import sleep
 import filter_contacts
-
 
 wishingContacts = filter_contacts.filter_contacts("google.csv")
 driver = webdriver.Firefox('C:\webdrivers')
@@ -37,53 +30,3 @@ for key, value in wishingContacts.items():
     except Exception:
         input_box.clear()
         continue
-    
-
-
-# ### for groups
-# span class = "i0jNr"
-
-# ### in chats
-# 
-# #### chat 2
-# 
-# div class = "_3m_Xw"
-# 
-# div tabindex = "-1"
-# 
-# div class = "_2nY6U"
-# 
-# div class = "_3OvU8"
-# 
-# div class = "_3vPI2"
-# 
-# div class = "zoWT4"
-# 
-# span class = "_3q9s6"
-# 
-# span class = "_ccCW FqYAR.i0jNr" dir = "auto" title = "zeeshan"
-
-# ## for chat 3
-# 
-# div class = "_3m_Xw"
-# 
-# div tabindex = "-1"
-# 
-# div class = "_2nY6U"
-# 
-# div class = "_3OvU8"
-# 
-# div class = "_3vPI2"
-# 
-# div class = "zoWT4"
-# 
-# span class = "_3q9s6"
-# 
-# span class = "_ccCW FqYAR i0jNr" dir = "auto" title = "Zeeshan Jessani Ksbl"
-# 
-
-# In[ ]:
-
-
-
-
