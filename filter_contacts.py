10a88b1c7c598e8e3209607df4c024619de9f2fc
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2022-03-17 11:14:15 +0500

    remove index from iteration in line 7
    filter_contacts.py:
    remove index from iteration in line 7
    add type hinting in function definition
    send_greetings.py:
    change constants to UPPER_CASE style
    remove commented code which wasn't doing anything
    remove input_box.clear() from line 31
    add exception that tells error at which key
    change line 6 from c:\webdrives to ./webdrivers for use on Linux system instead of Windows

--- filter_contacts.py
+++ filter_contacts.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
-def filter_contacts(filename):
+
+def filter_contacts(filename: str) -> dict:
     all_contacts = pd.read_csv(filename)
     all_contacts = all_contacts[["Name", "Given Name"]]
-    wishing_contacts = dict()
-    for index, row in all_contacts.iterrows():
-        print(str(row['Name'])+"  -  "+str(row['Given Name']))
-        wishing_contacts[str(row['Name'])] = str(row['Given Name'])
+    wishing_contacts = {}
+    for row in all_contacts.iterrows():
+        print(str(row["Name"]) + "  -  " + str(row["Given Name"]))
+        wishing_contacts[str(row["Name"])] = str(row["Given Name"])
     return wishing_contacts
--- send_greetings.py
+++ send_greetings.py
@@ -1,32 +1,27 @@
-from selenium import webdriver
 from time import sleep
+from selenium import webdriver
 import filter_contacts
 
 wishingContacts = filter_contacts.filter_contacts("google.csv")
-driver = webdriver.Firefox('C:\webdrivers')
-driver.get('https://web.whatsapp.com')
+driver = webdriver.Firefox("./webdrivers")
+driver.get("https://web.whatsapp.com")
 sleep(30)
-contacts = dict()
 
 for key, value in wishingContacts.items():
-    pre_msg = "Salam *"
-    post_msg = "* Eid mubarak to you and your family. _This message is automatically sent from my Eid-greeting bot program to my contact list. For more info visit my repo: https://github.com/sazk07/Eid-Auto-Wish-Bot_ *"
+    PRE_MSG = "Salam *"
+    POST_MSG = "* Eid mubarak to you and your family. _This message is automatically sent from my Eid-greeting bot program to my contact list. For more info visit my repo: https://github.com/sazk07/Eid-Auto-Wish-Bot_ *"
     input_box = None
     try:
         input_box = driver.find_element_by_css_selector("div[data-tab='3']")
         input_box.click()
         input_box.send_keys(key)
         sleep(0.7)
-        userbox = driver.find_element_by_css_selector("span[title='"+key+"']")
+        userbox = driver.find_element_by_css_selector("span[title='" + key + "']")
         userbox.click()
         inputbox = driver.find_element_by_css_selector("div[data-tab='6']")
         inputbox.click()
-        inputbox.send_keys(pre_msg+value+post_msg+'\n')
-        #sleep(1)
-        #send_button = driver.find_element_by_css_selector("span[data-icon='send']")
-        #send_button.click()
-        #sleep(1)
+        inputbox.send_keys(PRE_MSG + value + POST_MSG + "\n")
         continue
     except Exception:
-        input_box.clear()
+        print(f"Error! at {key}")
         continue
