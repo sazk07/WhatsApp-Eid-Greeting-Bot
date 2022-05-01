875df9b0ede7d583d2c407c5c554b8c9e6b9661e
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2022-05-02 00:49:59 +0500

    Add files via upload

--- send_greetings.py
+++ send_greetings.py
@@ -2,10 +2,10 @@ from selenium import webdriver
 from time import sleep
 import filter_contacts
 
-wishingContacts = filter_contacts.filter_contacts("./staging/test_contacts.csv")
+wishingContacts = filter_contacts.filter_contacts("./google.csv")
 options = webdriver.FirefoxOptions()
 driver = webdriver.Firefox(options=options)
-driver.get('https://web.whatsapp.com')
+driver.get("https://web.whatsapp.com")
 sleep(30)
 
 for key, value in wishingContacts.items():
@@ -15,11 +15,11 @@ for key, value in wishingContacts.items():
     name_box.click()
     name_box.send_keys(key)
     sleep(0.7)
-    user_box = driver.find_element_by_css_selector("span[title='"+key+"']")
+    user_box = driver.find_element_by_css_selector("span[title='" + key + "']")
     user_box.click()
     message_box = driver.find_element_by_css_selector("div[title='Type a message']")
     message_box.click()
-    message_box.send_keys(pre_msg+value+post_msg)
+    message_box.send_keys(pre_msg + value + post_msg)
     sleep(1)
     button_box = driver.find_element_by_xpath('//button[@class="_4sWnG"]')
     button_box.click()
