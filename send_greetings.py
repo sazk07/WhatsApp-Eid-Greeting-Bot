215c4360a997bdc58609567a47a382c7b2f9254c
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2022-05-02 00:59:16 +0500

    Add files via upload

--- send_greetings.py
+++ send_greetings.py
@@ -11,16 +11,20 @@ sleep(30)
 for key, value in wishingContacts.items():
     pre_msg = "Salam *"
     post_msg = "* Eid mubarak to you and your family. _This message is automatically sent from my Eid-greeting bot program to my contact list. For more info visit my repo: https://github.com/sazk07/WhatsApp-Eid-Greeting-Bot_ *"
+    # search for name
     name_box = driver.find_element_by_css_selector("div[data-tab='3'")
     name_box.click()
     name_box.send_keys(key)
     sleep(0.7)
+    # click on user name
     user_box = driver.find_element_by_css_selector("span[title='" + key + "']")
     user_box.click()
+    # click on message text box
     message_box = driver.find_element_by_css_selector("div[title='Type a message']")
     message_box.click()
     message_box.send_keys(pre_msg + value + post_msg)
     sleep(1)
+    # click send
     button_box = driver.find_element_by_xpath('//button[@class="_4sWnG"]')
     button_box.click()
     sleep(1)
