06ad2059b0dc817b786bfa9b9308f45009c8d699
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2022-07-09 13:53:16 +0500

    Update send_greetings.py

--- send_greetings.py
+++ send_greetings.py
@@ -1,5 +1,5 @@
-from selenium import webdriver
 from time import sleep
+from selenium import webdriver
 import filter_contacts
 
 wishingContacts = filter_contacts.filter_contacts("./google.csv")
@@ -10,9 +10,9 @@ sleep(30)
 
 for key, value in wishingContacts.items():
     pre_msg = "Salam *"
-    post_msg = "* Eid mubarak to you and your family. \n _This message is automatically sent from my Eid-greeting bot program to my contact list. If you want to view my source code visit my repo: https://github.com/sazk07/WhatsApp-Eid-Greeting-Bot_ "
+    POST_MSG = "* Eid mubarak to you and your family."
     # search for name
-    name_box = driver.find_element_by_css_selector("div[data-tab='3'")
+    name_box = driver.find_element_by_css_selector("div[data-tab='3']")
     name_box.click()
     name_box.send_keys(key)
     sleep(0.7)
@@ -23,14 +23,13 @@ for key, value in wishingContacts.items():
         # click on message text box
         message_box = driver.find_element_by_css_selector("div[title='Type a message']")
         message_box.click()
-        message_box.send_keys(pre_msg + value + post_msg)
+        message_box.send_keys(pre_msg + value + POST_MSG)
         sleep(1)
         # click send
         button_box = driver.find_element_by_xpath('//button[@class="_4sWnG"]')
         button_box.click()
         sleep(1)
-    except Exception():
+    except Exception:
         # when name is in contacts but not found in WhatsApp
         name_box.clear()
         continue
-    
