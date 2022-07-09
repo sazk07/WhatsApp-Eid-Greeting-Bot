6658f80d41917ccb7a61deedc8305f1b08b45b94
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2022-07-09 21:04:47 +0500

    Update send_greetings_attachments.py

--- send_greetings_attachments.py
+++ send_greetings_attachments.py
@@ -1,5 +1,8 @@
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import filter_contacts
 
 wishingContacts = filter_contacts.filter_contacts("./google.csv")
