835fd02cb3d6490b5a46013452608249bf10eb64
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2022-07-10 02:06:06 +0500

    mentioned changes made
    - changed button_box to send_button
    - renamed filter_contacts to filterContacts
    - try/except blocks

--- send_greetings_text.py
+++ send_greetings_text.py
@@ -1,35 +1,42 @@
 from time import sleep
 from selenium import webdriver
-import filter_contacts
+import filterContacts
 
-wishingContacts = filter_contacts.filter_contacts("./google.csv")
+PRE_MSG = "Salam *"
+ASTERISK_END = "* "
+POST_MSG = input("enter your message: ")
+wishing_contacts = filterContacts.filter_contacts("./google.csv")
 options = webdriver.FirefoxOptions()
 driver = webdriver.Firefox(options=options)
 driver.get("https://web.whatsapp.com")
-sleep(30)
+sleep(15)
 
-for key, value in wishingContacts.items():
-    pre_msg = "Salam *"
-    POST_MSG = "* Eid mubarak to you and your family."
+for key, value in wishing_contacts.items():
     # search for name
     name_box = driver.find_element_by_css_selector("div[data-tab='3']")
     name_box.click()
     name_box.send_keys(key)
-    sleep(0.7)
     try:
-        # click on user name
+        # search for user name in aside
         user_box = driver.find_element_by_css_selector("span[title='" + key + "']")
+    except NameError:
+        try:
+            sleep(1)
+        except NameError:
+            name_box.clear()
+            continue
+        else:
+            pass
+    else:
         user_box.click()
         # click on message text box
         message_box = driver.find_element_by_css_selector("div[title='Type a message']")
         message_box.click()
-        message_box.send_keys(pre_msg + value + POST_MSG)
-        sleep(1)
-        # click send
-        button_box = driver.find_element_by_xpath('//button[@class="_4sWnG"]')
-        button_box.click()
-        sleep(1)
-    except Exception:
-        # when name is in contacts but not found in WhatsApp
-        name_box.clear()
-        continue
+        message_box.send_keys(PRE_MSG + value + ASTERISK_END + POST_MSG)
+        try:
+            # click send
+            send_button = driver.find_element_by_xpath('//button[@class="_4sWnG"]')
+        except NameError:
+            sleep(1)
+        else:
+            send_button.click()
