b43de10fcc9cff09cdc3236b5e09b229a1fd329a
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2022-07-10 17:05:24 +0500

    changed to else:break from try/except for last for() loop

--- send_greetings_attachment_initial.py
+++ send_greetings_attachment_initial.py
@@ -4,16 +4,14 @@ import filterContacts
 
 spec_cases = input("enter names for separate messaging, separated by comma: ")
 spec_cases = spec_cases.split(",")
-# check file_path
-file_path = "./card.jpg"
-# edit msgs as needed before exec code
+file_path = "/home/sazk/unodrive/umbrella folders/python umbrella folder/eid wishbot/staging/notes/assets/card.jpg"
 PRE_MSG = "Salam *"
 ASTERISK_END = "* "
 POST_MSG = "Eid Mubarak to you and your family \nfrom Shahan"
 PRE_MSG2 = "Bonjour *"
 POST_MSG2 = "Selamat Hari Raya\nfrom Shahan"
 
-wishing_contacts = filterContacts.filter_contacts("./google.csv")
+wishing_contacts = filterContacts.filter_contacts("/home/sazk/unodrive/umbrella folders/python umbrella folder/eid wishbot/staging/notes/assets/google.csv")
 options = webdriver.FirefoxOptions()
 driver = webdriver.Firefox(options=options)
 driver.implicitly_wait(30)
@@ -29,8 +27,8 @@ for key, value in wishing_contacts.items():
     name_box.send_keys(key)
     try:
         # search for user name in aside
-        driver.implicitly_wait(10)
         sleep(1)
+        driver.implicitly_wait(10)
         user_box = driver.find_element_by_css_selector("span[title='" + key + "'")
         user_box.click()
     except exceptions.NoSuchElementException as err:
@@ -53,18 +51,18 @@ for key, value in wishing_contacts.items():
             sleep(1)
             message_box = driver.find_element_by_css_selector('div[data-tab="10"')
             message_box.click()
-            try:
-                for element in spec_cases:
-                    if key == element:
-                        message_box.send_keys(PRE_MSG2 + value + ASTERISK_END + POST_MSG2)
+            for element in spec_cases:
+                if key == element:
+                    message_box.send_keys(PRE_MSG2 + value + ASTERISK_END + POST_MSG2)
+                    # find send button
                     driver.implicitly_wait(10)
                     send_button = driver.find_element_by_css_selector('span[data-icon="send"')
                     send_button.click()
-            # i expect hell here. suggest improvements pls
-            except Exception as err3:
-                print(f"third unexpected {err3=}, {type(err3)=}")
-                message_box.send_keys(PRE_MSG + value + ASTERISK_END + POST_MSG)
-                # find send button
-                driver.implicitly_wait(10)
-                send_button = driver.find_element_by_css_selector('span[data-icon="send"')
-                send_button.click()
+                    # should i use else: pass to break out of the for loop?
+                else:
+                    break
+            message_box.send_keys(PRE_MSG + value + ASTERISK_END + POST_MSG)
+            # find send button
+            driver.implicitly_wait(10)
+            send_button = driver.find_element_by_css_selector('span[data-icon="send"')
+            send_button.click()
