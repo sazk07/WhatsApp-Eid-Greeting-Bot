a57a906596baf22d1fef548b829bc7477f9b6979
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2022-07-10 13:08:39 +0500

    changes to sleep and implicitly_wait functions
    - better try/except
    - better error handling
    - introduced sleep where implicitly_wait was not working

--- send_greetings_attachment_initial.py
+++ send_greetings_attachment_initial.py
@@ -1,48 +1,51 @@
 from time import sleep
-from selenium import webdriver
+from selenium import webdriver, common
 import filterContacts
 
-file_path = input("enter absolute file path to greeting card: ")
+file_path = "/home/sazk/unodrive/umbrella folders/python umbrella folder/eid wishbot/staging/notes/assets/card.jpg"
 PRE_MSG = "Salam *"
 ASTERISK_END = "* "
-POST_MSG = input("enter your message: ")
-wishing_contacts = filterContacts.filter_contacts("./staging/notes/assets/google.csv")
+POST_MSG = "Eid Mubarak to you and your family \n from Shahan"
+wishing_contacts = filterContacts.filter_contacts("/home/sazk/unodrive/umbrella folders/python umbrella folder/eid wishbot/staging/notes/assets/google.csv")
 options = webdriver.FirefoxOptions()
 driver = webdriver.Firefox(options=options)
+driver.implicitly_wait(30)
 driver.get("https://web.whatsapp.com")
-sleep(15)
 
 for key, value in wishing_contacts.items():
     # search for name
-    name_box = driver.find_element_by_css_selector("div[data-tab='3']")
+    sleep(1)
+    name_box = driver.find_element_by_css_selector("div[data-tab='3'")
     name_box.click()
     name_box.send_keys(key)
     try:
         # search for user name in aside
-        user_box = driver.find_element_by_css_selector("span[title='" + key + "']")
-    except NameError:
+        driver.implicitly_wait(10)
+        sleep(1)
+        user_box = driver.find_element_by_css_selector("span[title='" + str(key) + "'")
+        user_box.click()
+    except common.exceptions.NoSuchElementException as err:
+        print(f"unexpected {err=}, {type(err)=}")
+        name_box.clear()
+        continue
+    else:
+        # click on attach icon
         try:
-            sleep(1)
-        except NameError:
+            attach_item = driver.find_element_by_css_selector('span[data-icon="clip"]')
+            attach_item.click()
+        except common.exceptions.NoSuchElementException as err:
+            print(f"second unexpected {err=}, {type(err)=}")
             name_box.clear()
             continue
         else:
-            pass
-    else:
-        user_box.click()
-        # click on attach icon
-        attach_item = driver.find_element_by_css_selector('span[data-icon="clip"]')
-        attach_item.click()
-        try:
             attach_image = driver.find_element_by_xpath('//input[@type="file"]')
-        except NameError:
-            sleep(0.5)
-        finally:
             attach_image.send_keys(file_path)
             # find message box that comes with attachment
-            message_box = driver.find_element_by_css_selector('div[data-tab="10"]')
+            sleep(1)
+            message_box = driver.find_element_by_css_selector('div[data-tab="10"')
             message_box.click()
             message_box.send_keys(PRE_MSG + value + ASTERISK_END + POST_MSG)
             # find send button
-            send_button = driver.find_element_by_css_selector('span[data-icon="send"]')
+            driver.implicitly_wait(10)
+            send_button = driver.find_element_by_css_selector('span[data-icon="send"')
             send_button.click()
